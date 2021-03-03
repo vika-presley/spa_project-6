415730712279e55388011683cccafeee8921fb41
Viktoriia <v.a.fukleva@gmail.com>
2021-03-03 22:45:14 +0300

    add last block and footer

--- js/main.js
+++ js/main.js
@@ -38,5 +38,83 @@ $(document).ready(() => {
             heightStyle: "content"
         });
     } );
+
+    var myMap;
+    ymaps.ready(init);
+
+
+
+    function init () {
+
+        myMap = new ymaps.Map('map', {
+            center: [53.89, 27.52],
+            zoom: 15,
+            controls: []
+        });
+
+        myMap.controls.add('zoomControl', {
+            float: 'none',
+            position: {
+                right: 15,
+                top: 180
+            }
+        });
+
+        myMap.controls.get('zoomControl').options.set('size', 'small');
+
+        myMap.controls.add('geolocationControl', {
+            float: 'none',
+            position: {
+                right: 15,
+                top: 260
+            }
+        });
+
+            firstPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'ул. Жасминовая, д.ЗВ'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'images/gps.png',
+                iconImageSize: [16, 22],
+                iconImageOffset: [-15, -38]
+            });
+            secondPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'ул. Кирова, д.5'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'images/gps.png',
+                iconImageSize: [16, 22],
+                iconImageOffset: [140, 105]
+            });
+            thirdPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'ул. Братская, д.18'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'images/gps.png',
+                iconImageSize: [16, 22],
+                iconImageOffset: [-160, -175]
+            });
+            fourthPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'ул. Стеклянная, д.98'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'images/gps.png',
+                iconImageSize: [16, 22],
+                iconImageOffset: [-220, 0]
+            });
+
+        myMap.geoObjects
+            .add(firstPlacemark)
+            .add(secondPlacemark)
+            .add(thirdPlacemark)
+            .add(fourthPlacemark);
+
+
+
+        myMap.behaviors.disable('scrollZoom');
+        myMap.behaviors.disable('drag');
+
+    }
+
 });
 
